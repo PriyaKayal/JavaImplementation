79321590a4d85a5073764fd1b3bbcb480f6008ed
Gnanapriya Arumugam <Gnanapriya Arumugam>
2020-03-13 19:10:06 +0530

    JavaDate Class added

---
+++ 13-3-2020/JavaDate.java
@@ -0,0 +1,16 @@
+package com.oops;
+/**
+ *  author : Gnanapriya
+ *           Instantiate a Date object by invoking its constructor
+ */
+import java.util.Date;
+class DateClass {
+ public static void main(String args[]) {
+  Date objDate = new Date();
+  System.out.println(objDate.toString());
+ }
+}
+
+
+//output 
+//Fri Mar 13 19:06:16 IST 2020
