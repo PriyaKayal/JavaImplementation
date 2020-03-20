d2bdcb151dda1b05022a4c4dfddb6a2d4906c4d6
Gnanapriya Arumugam <Gnanapriya Arumugam>
2020-03-20 13:50:16 +0530

    OOPs Recollected

---
+++ 14-3-2020/OopsConcepts.java
@@ -0,0 +1,18 @@
+package com.oops;
+
+/**
+ * @author Gnanapriya 
+ * 		   Class, Object, Encapsulation, Abstraction, Polymorphism,
+ *         inheritance
+ */
+public class OopsConcepts {
+	/*
+	 * Class: It is like a skeleton of the objects
+	 * Objects: Everything is a object
+	 * like car, book etc.. 
+	 * Encapsulation: private members (info) hidden from unauthorized access, those can be activated by getter setter methods
+	 * Abstraction: using interface to achieve 100% abstraction
+	 * Polymorphism: static & dynamic
+	 * Inheritance: reusability
+	 */
+}
