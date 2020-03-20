ac701f3c9f33c4b2846edf94012b7948820f61fa
Gnanapriya Arumugam <Gnanapriya Arumugam>
2020-03-21 00:17:06 +0530

    StringBoolean added

---
+++ 19-3-2020/StringToBoolean.java
@@ -0,0 +1,14 @@
+package com.oops;
+/**
+ * @author Gnanapriya
+ * StringBoolean
+ */
+class MyStringToBoolean
+{
+public static void main(String a[])
+{
+String strBool = "true";
+Boolean bool = Boolean.parseBoolean(strBool);
+System.out.println(bool);
+}
+}
