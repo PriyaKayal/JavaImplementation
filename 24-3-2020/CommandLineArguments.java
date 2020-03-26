3020ff0cd0554cc71acb03ee8c9ee27f55b36f02
Gnanapriya Arumugam <Gnanapriya Arumugam>
2020-03-27 00:05:24 +0530

    CommandLineArguments added

---
+++ 24-3-2020/CommandLineArguments.java
@@ -0,0 +1,17 @@
+package com.oops;
+
+/**
+ * @author Gnanapriya 
+ * CommandLineArguments
+ */
+public class CommandLineArguments {
+	public static void main(String[] args) {
+		for (String s : args) {
+			System.out.println(s);
+		}
+	}
+}
+
+/*
+ * Output: Priya Kayal
+ */
